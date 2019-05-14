24491b1e8e33e336b3fb77957c0643e090fdc97d
openwell <timileyinojo@yahoo.com>
2019-05-14 16:25:21 +0100

    add resize

--- js/index.js
+++ js/index.js
@@ -4,17 +4,23 @@
 // }
 
 const element1 = document.querySelector(".intro").children[0];
-element1.addEventListener('mouseover', ()=>{
-    Object.assign(element1.style, { transform: 'scale(.6)', transition: 'transform 1s ease-in' });
-})
-element1.addEventListener('mouseout', ()=>{
-    Object.assign(element1.style, { transform: 'scale(1)' });
-})
+element1.addEventListener("mouseover", () => {
+  Object.assign(element1.style, {
+    transform: "scale(.6)",
+    transition: "transform 1s ease-in"
+  });
+});
+element1.addEventListener("mouseout", () => {
+  Object.assign(element1.style, { transform: "scale(1)" });
+});
 
-document.body.addEventListener('keydown', ()=>{
-    // window.scrollTo(0,document.querySelector(".footer").scrollHeight)
-    window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
-})
+document.body.addEventListener("keydown", () => {
+  // window.scrollTo(0,document.querySelector(".footer").scrollHeight)
+  window.scrollTo(
+    0,
+    document.body.scrollHeight || document.documentElement.scrollHeight
+  );
+});
 
 document.body.addEventListener('wheel', (event)=>{
 //   console.log(event.deltaX, event.deltaY, event.deltaZ)
@@ -23,5 +29,21 @@ document.body.addEventListener('wheel', (event)=>{
       element.style.fontSize = `${event.deltaX * 16}px`
   });
 })
+
+window.addEventListener("load", () => {
+  document.body.style.display = "none";
+  setTimeout(function() {
+    document.body.style.display = "block";
+  }, 3000);
+});
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth < 768){
+       document.body.style.backgroundColor = 'blue'
+    }
+    else{
+        document.body.style.backgroundColor = 'white'
+    }
+ });
 
 
